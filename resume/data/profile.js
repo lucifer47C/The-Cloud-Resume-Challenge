26fa14c153e24f19abf2a24570ecf2841fad8726
Steve Yadav <steveyadav.2002newdelhi@gmail.com>
2025-03-20 12:06:18 +0530

    Resume Added

---
+++ resume/data/profile.js
@@ -0,0 +1,110 @@
+const profileData = {
+  title: "Resume",
+  name: "Steve Yadav",
+  sub_title: "Cloud Engineer",
+  about: {
+    intro: `Cloud Engineer. Confident in building highly available, scaleable and secure architectures. Effective learner and collaborator dedicated to continuous learning and innovation.`,
+    contact: {
+      email: "steveyadav.2002newdelhi@gmail.com",
+      phone: "+91-9891157854",
+      address: "New Delhi, IN",
+    },
+  },
+  links: [
+    {
+      title: "LinkedIn",
+      src: "https://www.linkedin.com/in/steve-yadav",
+      iconClass: "fa-brands fa-linkedin-in",
+    },
+    {
+      title: "Github",
+      src: "https://github.com/lucifer47C",
+      iconClass: "fa-brands fa-github",
+    },
+  ],
+  skills: [
+    {
+      title: "Languages",
+      value: "C/C++, Java, Python, HTML5, CSS3, JavaScript",
+    },
+    {
+      title: "Frameworks/Libraries",
+      value: "ReactJS, React-Native, Redux, NodeJS-Express, NextJS",
+    },
+    {
+      title: "Others",
+      value: "Git/Github, Jenkins, AWS, Azure, Docker",
+    },
+  ],
+  experiences: [
+    {
+      organization: "IBM",
+      title: "Project Intern",
+      date: "Jun 2024 - Jul 2024",
+      details: [
+        `Developed a web application to implement a disaster recovery solution, ensuring data availability and resilience.`,
+        `100% deployment on AWS and using services like S3, EC2, CloudWatch and RDS.`,
+        `0% human intervention for disaster recovery.`,
+        ],
+    },
+    {
+      organization: "DRDO",
+      title: `Intern`,
+      date: "Jun 2024 - Jul 2024",
+      details: [
+        ` Explored IBM Quantum Simulator and Qiskit to make quantum circuits and implement Quantum algorithms.`,
+        ` Learnt about various quantum algorithms like Shor’s algorithm and Grover search algorithm.`,  
+      ],
+    },
+    {
+      organization: "Hara Jeevan",
+      title: `Volunteer Intern`,
+      date: "Jun 2022 - JuL 2022",
+      details: [
+        `  100% Managed their website: http://harajeevan.org/`,
+        `  Manually migrated the whole website without the help of any plugins.`,
+        `  Involved WordPress, MySQL, HTML, CSS etc.`,
+      ],
+    },
+  ],
+  projects: [
+    {
+      title: "DevOps-QR-code",
+      duration: "Mar 2025",
+      link: "https://github.com/lucifer47C/DevOps-Practice",
+      desc: ` To get hands-on with DevOps practices like containerization, CI/CD and monitoring.`,
+    },
+    {
+      title: "TrackNClassify",
+      duration: "Sep - Dec 2024",
+      link: "https://github.com/lucifer47C/TrackNClassify-Major",
+      desc: ` • Developed a multilayered authentication system, reducing unauthorized access attempts by 90%, integrating QR code scanning, and RFID for secure access management.<br>
+              • Responsible for integrating with cloud services, ensuring 99.9% uptime and with IOT components for seamless access management across multiple locations.<br>
+              • Tools Used: MERN stack (MongoDB, Express.js, React, and Node.js).`,
+    },
+    {
+      title: "FitZee",
+      duration: "Feb - Jul 2024",
+      link: "https://github.com/lucifer47C/FitZee-Minor",
+      desc: ` • An android application for Body Mass Index (BMI) calculation, and to estimate the calorie count of their meals through image recognition.<br>
+              • Responsible for 100% user interface creation using Java and XML.<br>
+              • Tools Used: Android Studio, Java, XML, Firebase.`,
+    },
+  ],
+  education: [
+    {
+      alma: "UPES, Dehradun",
+      duration: "2021 - 2025",
+      std: "B.Tech. (Computer Science & Engineering)",
+      score: "8.10 CGPA",
+    },
+  ],
+  certifications: [
+    {
+      desc: `<strong>AWS Certified Solutions Architect - Associate</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.credly.com/badges/d58cbe69-3392-4feb-a767-9c6380df237f/public_url'>https://goo.gl/RGRzVn</a>)`,
+      date: "Aug 2024",
+    },
+  ],
+  events: [],
+};
